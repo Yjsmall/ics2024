8ddd8c1a254d8c2a4ce983546519daf75a044504
Fisher <1186907891@qq.com>
2025-03-05 22:12:36 +0800

    add lldb debugger

--- nemu/scripts/native.mk
+++ nemu/scripts/native.mk
@@ -41,6 +41,10 @@ gdb: run-env
 	$(call git_commit, "gdb NEMU")
 	gdb -s $(BINARY) --args $(NEMU_EXEC)
 
+lldb: run-env
+	$(call git_commit, "gdb NEMU")
+	lldb -s $(BINARY) -- $(NEMU_EXEC)
+	
 clean-tools = $(dir $(shell find ./tools -maxdepth 2 -mindepth 2 -name "Makefile"))
 $(clean-tools):
 	-@$(MAKE) -s -C $@ clean
